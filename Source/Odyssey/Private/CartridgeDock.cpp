7a39ac58d04b45113f5a31798d1df05218009fa8
unknown <aziz.saydaminov02@gmail.com.>
2024-03-08 20:09:54 -0600

    Adds head bouncing, adds grabbable translation limit (still needs work).

--- Source/Odyssey/Private/CartridgeDock.cpp
+++ Source/Odyssey/Private/CartridgeDock.cpp
@@ -9,10 +9,15 @@ ACartridgeDock::ACartridgeDock()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	RailPoint= CreateDefaultSubobject<USceneComponent>(TEXT("RailTrigger"));
-	RailTrigger= CreateDefaultSubobject<UBoxComponent>(TEXT("RailPoint"));
+	RailPoint = CreateDefaultSubobject<USceneComponent>(TEXT("RailPoint"));
+	RailTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("RailTrigger"));
+	CustomPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CustomPlane"));
+
+	RootComponent = RailPoint;
+	RailTrigger->SetupAttachment(RailPoint);
 
 	RailTrigger->SetGenerateOverlapEvents(true);
+	RailTrigger->OnComponentBeginOverlap.AddDynamic(this, &ACartridgeDock::BeginOverlap);
 }
 
 // Called when the game starts or when spawned
@@ -26,7 +31,36 @@ void ACartridgeDock::BeginPlay()
 void ACartridgeDock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
 
+void ACartridgeDock::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	FBodyInstance* BodyInstance = OtherComp->GetBodyInstance();
+	if (BodyInstance)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, BodyInstance->GetBodyDebugName());
+
+		// Define the parameters of the custom plane
+		FVector CustomPlaneNormal = CustomPlane->GetForwardVector(); // Normal vector of the custom plane
+
+		// Set the DOF mode to CustomPlane
+		BodyInstance->DOFMode = EDOFMode::CustomPlane;
+
+		// Set the parameters of the custom plane
+		BodyInstance->CustomDOFPlaneNormal = CustomPlaneNormal;
+
+		OtherComp->BodyInstance.CreateDOFLock();
+	}
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, OtherComp->GetName());
+
+	//OtherComp->GetBodyInstance()->bLockRotation = true;
+
+	//OtherComp->GetBodyInstance()->bLockXTranslation = true;
+	//OtherComp->GetBodyInstance()->bLockZTranslation = true;
+}
+
+void ACartridgeDock::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
 
--- Source/Odyssey/Public/CartridgeDock.h
+++ Source/Odyssey/Public/CartridgeDock.h
@@ -30,6 +30,20 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UBoxComponent* RailTrigger;
 
-	void InitializeRail();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* CustomPlane;
 
+    UFUNCTION( )
+    void BeginOverlap(UPrimitiveComponent* OverlappedComponent, 
+                      AActor* OtherActor, 
+                      UPrimitiveComponent* OtherComp, 
+                      int32 OtherBodyIndex, 
+                      bool bFromSweep, 
+                      const FHitResult &SweepResult );
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex);
 };
